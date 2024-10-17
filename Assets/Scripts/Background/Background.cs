c3cbf9fe16b3ea691d2956ce5e80ca1b2b51c5c4
simple2126 <kimcw212@gmail.com>
2024-10-17 18:09:20 +0900

    feat: Tilemap 스크롤 추가
    Tilemap 스크롤 함수 스크립트 추가
    Tilemap 이미지 변경 위아래 1칸씩 추가

---
+++ Assets/Scripts/Background/Background.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float scrollAmount; // 두 배경 사이의 거리
+    [SerializeField] private float moveSpeed;
+
+    private Vector3 moveDirection = new Vector3(0f, -1f, 0f);
+
+    private void Update()
+    {
+        // 
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        
+        // 배경이 설정 범위 벗어나면 위치 재설정
+        if(transform.position.y <= -scrollAmount)
+        {
+            transform.position = target.position - moveDirection * scrollAmount;
+        }
+    }
+}
