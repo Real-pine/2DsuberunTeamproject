8f233bb64e9dbae5969ca5b1df25a33661d00546
Real-pine <crewger9030@gmail.com>
2024-10-17 19:56:37 +0900

    feat: RangkingUI에 순위출력기능 구현
    PlayerPrefs에 저장된 데이터를 통해 RankingUI패널에 내림차순으로 정렬된 순위를 출력

--- Assets/Scripts/UI/RankingPanelUI.cs
+++ Assets/Scripts/UI/RankingPanelUI.cs
@@ -2,8 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class RankingPanelUI : MonoBehaviour
 {
-    // TODO : 스코어랭킹 데이터를 텍스트로 출력하는 함수 구현
+    [SerializeField] private TMP_Text[] scoreText;
+    [SerializeField] private TMP_Text[] nameText;
+
+    // TODO : 저장된 이름과 최고점수들을 RankingBoxPanel에 Text로 쭉 나열
+    //      대신 내림차순으로 나열해야하고, 새로운 최고 점수가 등장하면 
+    private void Start()
+    {
+        UpdateRankingUI();
+    }
+
+    private void UpdateRankingUI()
+    {
+        for (int i = 0; i< 7; i++)
+        {
+            string bestName = PlayerPrefs.GetString(i + "BestName");
+            float bestScore = PlayerPrefs.GetFloat(i + "BestScore");
+
+            nameText[i].text = (i + 1) + ". " + bestName;
+            scoreText[i].text = bestScore.ToString() + " M";
+        }
+    }
 }
