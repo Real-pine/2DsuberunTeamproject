6caeda7f4128fc6aab3ffc67a1bf53430210f144
leecoading <leehy011011@gmail.com>
2024-10-18 15:46:35 +0900

    fix OptionPanel
    프리팹화 해둔 옵션
    play game scene에 적용 및
    정상 작동 확인
    (사운드 및 help버튼은 추후
    완성 시 구현)

--- Assets/Scripts/Util/OptionPanelBtn.cs
+++ Assets/Scripts/Util/OptionPanelBtn.cs
@@ -19,16 +19,11 @@ public class OptionPanelBtn : MonoBehaviour
 
     public void OpenOptionPanel()
     {
-        optionPanel.SetActive(!optionPanel.activeSelf);
+        // 현재 패널의 활성 상태를 반전
+        bool isActive = optionPanel.activeSelf;
+        optionPanel.SetActive(!isActive); // 패널 열기/닫기
 
-        if (optionPanel.activeSelf)
-        {
-            Time.timeScale = 0; // 게임 일시 정지
-        }
-
-        else
-        {
-            Time.timeScale = 1;
-        }
+        // 패널이 활성화되면 게임 일시 정지, 그렇지 않으면 재개
+        Time.timeScale = isActive ? 1 : 0; // isActive가 true면 1, false면 0
     }
 }
